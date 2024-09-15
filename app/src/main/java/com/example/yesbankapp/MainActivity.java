207eee79cba3dfb0c362befbe43e2ea9d5e231cc
Atul Prajapati <ap9059566@gmail.com>
2024-09-15 08:39:29 +0530

    Update

--- app/src/main/java/com/example/yesbankapp/MainActivity.java
+++ app/src/main/java/com/example/yesbankapp/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import com.google.firebase.FirebaseApp;
@@ -30,6 +31,9 @@ public class MainActivity extends AppCompatActivity implements SmsReceiver.UPILi
         setContentView(R.layout.activity_main);
 
         upiTextView = findViewById(R.id.upiTextView);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
 
         // Initialize Firebase
         FirebaseApp.initializeApp(this);
